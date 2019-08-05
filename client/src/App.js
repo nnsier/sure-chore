01697bc8ec40f9fe11a0a0a90382ecf30ce37a6f
ecthyr <arreisnahtan@gmail.com>
2019-08-05 18:14:23 -0400

    setup basic server and client connection

---
+++ client/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      Don't learn React. Learn server-side shit.
+    </div>
+  );
+}
+
+export default App;
